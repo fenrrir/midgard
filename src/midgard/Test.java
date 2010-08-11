468c067d291f31de7dcfabf3862c2a4829e07127
Rodrigo Araújo <fenrrir@gmail.com>
2010-08-11 18:20:10 -0300

    Commit inicial

---
+++ src/midgard/Test.java
@@ -0,0 +1,34 @@
+package midgard;
+
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.midlet.MIDletStateChangeException;
+
+
+public class Test extends MIDlet {
+
+    protected void startApp() throws MIDletStateChangeException {
+        System.out.println("Hello World");
+        // TODO
+    }
+
+    protected void pauseApp() {
+        // This is not currently called by the Squawk VM
+    }
+
+    /**
+     * Called if the MIDlet is terminated by the system.
+     * I.e. if startApp throws any exception other than MIDletStateChangeException,
+     * if the isolate running the MIDlet is killed with Isolate.exit(), or
+     * if VM.stopVM() is called.
+     * 
+     * It is not called if MIDlet.notifyDestroyed() was called.
+     *
+     * @param unconditional If true when this method is called, the MIDlet must
+     *    cleanup and release all resources. If false the MIDlet may throw
+     *    MIDletStateChangeException  to indicate it does not want to be destroyed
+     *    at this time.
+     */
+    protected void destroyApp(boolean unconditional) throws MIDletStateChangeException {
+        // TODO
+    }
+}
