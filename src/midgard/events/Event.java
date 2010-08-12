25fea1db5647afef9f30e662fe4ba2004ff4ee2e
Rodrigo Araújo <fenrrir@gmail.com>
2010-08-11 21:40:16 -0300

    Adicionando class base concreta para eventos

---
+++ src/midgard/events/Event.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package midgard.events;
+
+/**
+ *
+ * @author fenrrir
+ */
+public class Event implements IEvent{
+    private Object content;
+
+    public Event(Object content) {
+        this.content = content;
+    }
+
+
+
+    public Object getContentObject() {
+        return this.content;
+    }
+
+    public long getID() {
+        return this.hashCode();
+    }
+
+    public long getType() {
+        return this.getClass().hashCode();
+    }
+}
