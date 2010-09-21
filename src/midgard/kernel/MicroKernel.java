88efa5da86e3b5432c569080add218aa08cd9394
Rodrigo Araújo <fenrrir@gmail.com>
2010-09-21 12:23:36 -0300

    Adicionado boot do middleware

--- src/midgard/kernel/MicroKernel.java
+++ src/midgard/kernel/MicroKernel.java
@@ -5,7 +5,13 @@
 
 package midgard.kernel;
 
+import midgard.componentmodel.IComponent;
+import midgard.componentmodel.IProxyComponent;
+import midgard.components.ComponentManager;
+import midgard.components.IComponentManager;
 import midgard.naming.INameService;
+import midgard.repositories.ComponentRepositoryManager;
+import midgard.repositories.IComponentRepositoryManager;
 
 /**
  *
@@ -14,12 +20,56 @@ import midgard.naming.INameService;
 public class MicroKernel {
 
     private static MicroKernel instance = null;
+    private ComponentManager componentManager;
 
     private MicroKernel(){
+        boot();
+    }
+
+
+    private void boot(){
+
+        
+        IComponentRepositoryManager repositoryManager;
+        ComponentRepositoryManager repositoryProxy;
+
+        IComponentManager componentManagerImpl;
+
+
+        repositoryProxy = ComponentRepositoryManager.getDefault();
+
+        repositoryProxy.load();
+        repositoryProxy.initialize();
+
+        
+        repositoryManager = (IComponentRepositoryManager)
+           repositoryProxy.getImplementationOfInterface("IComponentRepositoryManager");
+        
+        if ( !repositoryProxy.getConcreteComponent().getName().equals(repositoryManager.getName()) ){
+            repositoryProxy.pause();
+            repositoryManager.destroy();
+            repositoryProxy.setConcreteComponent(repositoryManager);
+            repositoryProxy.load();
+            repositoryProxy.initialize();
+        }
+
+
+        componentManager = (ComponentManager) 
+                        repositoryManager.getProxyOf("IComponentManager");
+
+        componentManagerImpl = (IComponentManager)
+                repositoryManager.getImplementationOfInterface("IComponentManager");
+
+
+        componentManager.setConcreteComponent(componentManagerImpl);
+        componentManager.load();
+        componentManager.connect("IComponentRepositoryManager", repositoryProxy);
+        componentManager.initialize();
+
     }
 
     public INameService getNameService(){
-        return null;
+        return (INameService) componentManager.resolveComponent("INameService");
     }
 
     public ClassLoader getClassLoader(){
