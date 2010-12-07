e73730e063c5ba788950947f75fdd87f7fc6a132
Rodrigo Araújo <fenrrir@gmail.com>
2010-12-07 11:50:53 -0300

    Adicionando constantes dos estados da bateria

--- src/midgard/sensors/battery/IBatterySensor.java
+++ src/midgard/sensors/battery/IBatterySensor.java
@@ -5,6 +5,7 @@
 
 package midgard.sensors.battery;
 
+import com.sun.spot.peripheral.IBattery;
 import midgard.battery.IBatteryManager;
 import midgard.sensors.ISensor;
 
@@ -13,6 +14,13 @@ import midgard.sensors.ISensor;
  * @author fenrrir
  */
 public interface IBatterySensor extends ISensor{
+
+    public final byte CHARGING = IBattery.CHARGING;
+    public final byte DEAD_BATTERY = IBattery.DEAD_BATTERY;
+    public final byte DISCHARGING = IBattery.DISCHARGING;
+    public final byte EXT_POWERED = IBattery.EXT_POWERED;
+    public final byte LOW_BATTERY = IBattery.LOW_BATTERY;
+    public final byte NO_BATTERY = IBattery.NO_BATTERY;
     public IBatteryManager getManager();
 
 }
