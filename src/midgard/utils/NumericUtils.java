cf7001a79ec33469ecedd005192356fcf308926f
Rodrigo Araújo <fenrrir@gmail.com>
2010-12-09 14:05:24 -0300

    Classe utilitária para comparação de números

---
+++ src/midgard/utils/NumericUtils.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package midgard.utils;
+
+/**
+ *
+ * @author fenrrir
+ */
+public class NumericUtils {
+
+    public static boolean lt(double a, double b){
+        return a < b;
+    }
+
+    public static boolean gt(double a, double b){
+        return a > b;
+    }
+
+    public static boolean eq(double a, double b){
+        return a == b;
+    }
+
+}
