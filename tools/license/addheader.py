f8aad4c4e1afdd8295be0c81e7cca78c11ba30b7
Rodrigo Araújo <fenrrir@gmail.com>
2011-03-19 10:55:43 -0300

    Adicionando ferramenta para inserir licença

---
+++ tools/license/addheader.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import os
+import sys
+
+NETBEANS_HEADER="""/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */"""
+
+
+def get_header():
+    dirname = os.path.dirname(__file__)
+    headerfile = os.path.join(dirname, "header.txt")
+
+    with file(headerfile) as f:
+        content = f.read()
+
+    return content
+
+
+HEADER = get_header()
+
+def add_license(filename):
+
+    if not filename.endswith(".java"):
+        return
+
+    with file(filename) as f:
+        content = f.read()
+
+    if content.startswith(NETBEANS_HEADER):
+        print filename
+        content = content.split("\n")[4:] # remove header
+        content = "\n".join(content)
+        newcontent = HEADER + content
+
+        with file(filename, "w") as f:
+            f.write(newcontent)
+
+
+
+def main():
+
+    for root, dirs, files in os.walk(sys.argv[1]):
+        for filename in files:
+
+            fullfilename = os.path.join(root, filename)
+            add_license(fullfilename)
+
+
+if __name__ == "__main__":
+    main()
